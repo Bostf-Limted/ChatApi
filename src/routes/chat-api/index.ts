fdff2c41831dd152f2f5dc7b952d3345cf1adc66
BsoftLimited <okelekelenoebl@gmail.com>
2024-04-26 08:46:21 +0100

    I forgot what I was doing

--- src/routes/chat-api/index.ts
+++ src/routes/chat-api/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import expressWS from "express-ws";
+import jetLogger from "jet-logger";
+
 import userRouter from "./users";
 import groupRouter from "./groups";
 import chatRouter from "./chats";
 
 const api = express();
+
+const { app: wsApp } = expressWS(api);
+
+wsApp.ws("/", (ws, req) =>{
+    jetLogger.info("user connected");
+    ws.on("message", (message: string)=>{
+
+    });
+
+    ws.on("close", ()=>{
+        jetLogger.info("user left");
+    });
+});
+
 
 api.use("/users", userRouter);
 api.use("/groups", groupRouter);
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import logger from 'jet-logger';
 
 import api from './app';
 
-const port = Number.parseInt(process.env.PORT || "5000");
+const port = Number.parseInt(process.env.PORT || "4000");
 const SERVER_START_MSG = ('Express server started on port: ' + port);
 const server = api.listen(port, () =>{
   logger.info(SERVER_START_MSG);
